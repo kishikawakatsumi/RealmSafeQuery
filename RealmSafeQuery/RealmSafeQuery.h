aa72f9fed34bac9883b508a9c9425a55e78462b7
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2017-09-12 17:45:24 +0900

    Initial commit

---
+++ RealmSafeQuery/RealmSafeQuery.h
@@ -0,0 +1,12 @@
+//
+//  RealmSafeQuery.h
+//  RealmSafeQuery
+//
+//  Created by Kishikawa Katsumi on 2017/09/12.
+//  Copyright © 2017 Realm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double RealmSafeQueryVersionNumber;
+FOUNDATION_EXPORT const unsigned char RealmSafeQueryVersionString[];
---
+++ RealmSafeQuery/Results+KeyPath.swift
@@ -0,0 +1,24 @@
+//
+//  Results+KeyPath.swift
+//  RealmSafeQuery
+//
+//  Created by Kishikawa Katsumi on 2017/09/12.
+//  Copyright © 2017 Realm. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+extension Results {
+    public func filter<P: Predicate>(_ expression: @autoclosure () -> P) -> Results<T> where P.ObjectType == T {
+        return filter(expression().predicate)
+    }
+
+    public func sorted<RealmObject: Object, RealmProperty: RealmPropertyType>(byKeyPath keyPath: KeyPath<RealmObject, RealmProperty>, ascending: Bool = true) -> Results<T> {
+        return sorted(by: [SortDescriptor(keyPath: keyPath._kvcKeyPathString!, ascending: ascending)])
+    }
+
+    public func sorted<RealmObject: Object, RealmProperty: RealmPropertyType>(byKeyPath keyPath: KeyPath<RealmObject, RealmProperty?>, ascending: Bool = true) -> Results<T> {
+        return sorted(by: [SortDescriptor(keyPath: keyPath._kvcKeyPathString!, ascending: ascending)])
+    }
+}
